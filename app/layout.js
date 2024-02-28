e7b33fe821a8c0ea3e1959019b966d44a99889dc
Noah Nakell <noah@noahnakell.com>
2024-02-27 19:48:19 -0700

    fixed display error

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <head>
-        <title>{title}</title>
+        <title>{metadata.title}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
